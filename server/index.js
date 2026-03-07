2ab8a23f76268374847c2be69214a6611d4473e9
Max Lui <zsm3ham@gmail.com>
2026-03-07 18:17:31 +0000

    testing cors

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: { origin: "*" }
+    cors: { origin: "https://chasing-seven.vercel.app" }
 });
 
 // ─── Game Logic ───────────────────────────────────────────────────────────────
